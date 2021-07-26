419ec08de4c0dc4316c657132215ebe39a8464c2
johnnysee <js.johnny.see@gmail.com>
2021-07-26 18:44:30 +0200

    First commit of my_group

---
+++ my_group.rb
@@ -0,0 +1,19 @@
+# 1. Create an array and store it in a variable called my_group
+my_group = []
+
+# 2. Create three hashes describing an individual and name them person_1, person_2 and person_3. The hash should contain the following keys:
+# 3. name
+# 4. gender
+# 5. age
+
+person_1 = {name: "Johnny", gender: "male", age: 29}
+
+person_2 = {name: "Emelie", gender: "female", age: 28}
+
+person_3 = {name: "Celine", gender: "female", age: 2}
+
+# 6. Add all people to your my_group array.
+
+my_group = [person_1, person_2, person_3]
+
+# 7. Do everything in IRB but also save it into your my_group.rb. Commit everything and push it up to your Github Repo. We'll be returning to this example later on.
